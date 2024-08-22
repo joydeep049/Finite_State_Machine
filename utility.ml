67eded111bb0d7b3e04d98d6ddb5f1adc4ce72d5
joydeep049 <bntripathy123@gmail.com>
2024-08-22 11:51:04 +0530

    feat: added utility function

--- utility.ml
+++ utility.ml
@@ -1,2 +1,8 @@
 let is_accepting (final_list : state list) (s:state) =
     List.mem s final_list
+
+let char_to_event (c:char) : event option =
+    match c with
+        | 'a' -> Some A
+        | 'b' -> Some B
+        |  _  -> None
